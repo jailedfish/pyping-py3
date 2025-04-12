7a8aafa66b89e38f9a24d27f524fd9eb3ce2758e
cabup <pass@localhost>
2025-04-13 01:04:32 +0300

    real v0.0.7

--- setup.py
+++ setup.py
@@ -5,25 +5,25 @@ import os
 import sys
 
 try:
-	from setuptools import setup
+    from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
 setup(
-	name='pyping',
-	version='0.0.6',
-	description='A pure python ICMP ping implementation using raw sockets',
-	long_description=open('README.rst').read() + '\n\n' +
+    name='pyping-py3',
+    version='0.0.9',
+    description='A pure python ICMP ping implementation using raw sockets',
+    long_description=open('README.rst').read() + '\n\n' +
                      open('HISTORY.rst').read(),
-	license=open("LICENSE").read(),
-	author="toxinu",
-	author_email="toxinu@gmail.com",
-	url='https://github.com/toxinu/pyping/',
-	keywords="ping icmp network latency",
-	packages = ['pyping'],
-	scripts=["bin/pyping"]
+    license=open("LICENSE").read(),
+    author="toxinu",
+    author_email="toxinu@gmail.com",
+    url='https://github.com/jailedfish/pyping-py3/',
+    keywords="ping icmp network latency",
+    packages = ['pyping_py3'],
+    scripts=["bin/pyping_py3"]
 )
